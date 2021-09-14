b7b0c032cb50f2406d6203e66d1aec6c784cd90e
Adria Meca <amecamon7@gmail.com>
2021-09-14 21:54:10 +0200

    Procedures that remove items from lists have been added to the module 'array_procedures'.

--- modules/array_procedures.f90
+++ modules/array_procedures.f90
@@ -23,9 +23,30 @@ module array_procedures
     module procedure add_int, add_int_list, add_int_pair
   end interface add_item
 
-  public add_item
+  public add_item, del_int, pop
 
 contains
+  !Function that returns a list without the element placed at index.
+  function pop(list, index) result(reduced_list)
+    implicit none
+
+    double precision, dimension(:), intent(in) :: list
+    integer, intent(in) :: index
+
+    double precision, dimension(:), allocatable :: reduced_list
+    integer :: n
+
+    n = size(list)
+    if ((1 <= index).and.(index <= n)) then
+      allocate(reduced_list(n-1))
+      reduced_list = (/ list(:index-1), list(index+1:) /)
+    else
+      allocate(reduced_list(n))
+      reduced_list = list
+    end if
+  end function pop
+
+
   !Subroutine that adds an integer to an array of integers.
   subroutine add_int(list, element)
     implicit none
@@ -114,4 +135,15 @@ contains
       list(1) = element
     end if
   end subroutine add_int_pair
+
+
+  !Subroutine that removes an integer from a list of integers.
+  subroutine del_int(list, element)
+    implicit none
+
+    integer, dimension(:), allocatable, intent(inout) :: list
+    integer, intent(in) :: element
+
+    if (allocated(list)) list = pack(list, mask=list/=element)
+  end subroutine del_int
 end module array_procedures
