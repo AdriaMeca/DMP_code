91d26d1ed9c3fb5a2a1e592d52e6a08bae989bea
Adria Meca <amecamon7@gmail.com>
2021-11-23 07:08:31 +0100

    I've added a subroutine that performs a uniform algorithm.

--- modules/network_dynamics.f90
+++ modules/network_dynamics.f90
@@ -82,9 +82,9 @@ contains
     type(node), dimension(:), intent(inout) :: network
 
     double precision :: r1, r2(2), r3(2)
-    integer :: i, index_i, j, k, m
+    integer :: i, idx, j, k, m
 
-    do index_i = 1, N*c/4
+    do idx = 1, N*c/4
       call random_number(r1)
 
       if (r1 < q) then
@@ -122,4 +122,58 @@ contains
       end if
     end do
   end subroutine std_rewiring
+
+
+  !Subroutine that modifies the connections of a network using a uniform rewiring
+  !algorithm.
+  subroutine uni_rewiring(network, N, c, q)
+    implicit none
+
+    double precision, intent(in) :: q
+    integer, intent(in) :: c, N
+    type(node), dimension(:), intent(inout) :: network
+
+    double precision :: r1, r2(2), r3, r4
+    integer :: i, idx, j, jsize, k, m
+
+    do idx = 1, N*c/4
+      call random_number(r1)
+
+      if (r1 < q) then
+        do while (.true.)
+          call random_number(r2)
+
+          !We choose two random nodes from the network.
+          i = 1 + floor(N*r2(1))
+          k = 1 + floor(N*r2(2))
+
+          !If i and k are different and disconnected, we proceed.
+          if ((i /= k).and.(all(network(i)%neighbors /= k))) then
+            call random_number(r3)
+
+            !We choose a random node from the network.
+            j = 1 + floor(N*r3)
+
+            !If j has neighbors we proceed.
+            jsize = size(network(j)%neighbors)
+            if (jsize > 0) then
+              call random_number(r4)
+
+              !We choose a random neighbor of j.
+              m = network(j)%neighbors(1 + floor(jsize*r4))
+              exit
+            end if
+          end if
+        end do
+
+        !We remove the old connection.
+        call del_int(network(j)%neighbors, m)
+        call del_int(network(m)%neighbors, j)
+
+        !We add the new connection.
+        call add_item(network(i)%neighbors, k)
+        call add_item(network(k)%neighbors, i)
+      end if
+    end do
+  end subroutine uni_rewiring
 end module network_dynamics
