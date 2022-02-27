0af37582ca1c3a5632c3db80d14f3bcf2480047a
Adria Meca <amecamon7@gmail.com>
2022-02-27 19:29:08 +0100

    I've added two functions: one that calculates the sum over the degree of each node in a network and another that calculates the number of edges in a network.

--- modules/network_properties.f90
+++ modules/network_properties.f90
@@ -6,7 +6,7 @@ module network_properties
 
   private
 
-  public mean_degree
+  public edges, mean_degree
 
 contains
   !Function that calculates the mean degree of a network.
@@ -15,9 +15,35 @@ contains
 
     type(node), dimension(:), intent(in) :: network
 
-    integer :: i, N
+    integer :: N
 
     N = size(network)
-    mean_degree = sum((/ (size(network(i)%neighbors), i=1,N) /)) / dble(N)
+    mean_degree = connection_points(network) / dble(N)
   end function mean_degree
+
+
+  !Function that calculates the total number of connection points in a network.
+  !In other words, it computes the sum over the degree of each node in a graph.
+  integer function connection_points(network)
+    implicit none
+
+    type(node), dimension(:), intent(in) :: network
+
+    integer :: i
+
+    connection_points = 0
+    do i = 1, size(network)
+      connection_points = connection_points + size(network(i)%neighbors)
+    end do
+  end function connection_points
+
+
+  !Function that calculates the number of edges of a network.
+  integer function edges(network)
+    implicit none
+
+    type(node), dimension(:), intent(in) :: network
+
+    edges = connection_points(network) / 2
+  end function edges
 end module network_properties
