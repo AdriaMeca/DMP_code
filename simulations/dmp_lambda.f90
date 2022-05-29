0f43b7987f11e669ed17194ee017c543e3bf8616
DrMeca <amecamon7@gmail.com>
2022-05-30 01:14:09 +0200

    I've generalized the main simulation to be able to study the patient zero problem as we vary any parameter that we want.

--- simulations/dmp_lambda.f90
+++ simulations/dmp_lambda.f90
@@ -1,33 +1,28 @@
 !Simulation that aims to test the efficiency of the DMP(r) algorithm by studying
 !the precision with which it locates the true patient zero(s) in a network at a
-!certain time t as we vary the lambda parameter (with all the others fixed).
+!certain time t as we vary one of the parameters, leaving all the others fixed.
 !Author: Adria Meca Montserrat.
-!Last modified date: 26/05/22.
-program dmp_lambda
-  use array_procedures, only : find, int_llist, my_pack, quicksort
-  use dmp_algorithms, only : dmp
-  use mc_simulations, only : mc_sim
-  use network_generation, only : node, RRG
+!Last modified date: 29/05/22.
+program patient_zero
+  use array_procedures, only : int_llist
+  use network_generation, only : node, PN, RRG
+  use patient_zero_problem, only : pz_sim
   use random_number_generator, only : r1279, setr1279
   use rewiring_algorithms, only : rewiring
 
   implicit none
 
   !Variables.
-  character(len=1), dimension(:), allocatable :: states
-  character(len=9) :: model
+  character(len=9) :: graph, model, param, scale
 
-  double precision, dimension(:, :), allocatable :: tmp_dmp_probs, tmp_mc_probs
-  double precision, dimension(:), allocatable :: energies, ps, pe, pi, pr
-  double precision, parameter :: a=5.0d-2, b=1.0d0-a, small=1.0d-300
-  double precision :: alpha, avg_g, avg_prob, avg_prob2, avg_rank, avg_rank2, &
-    err_prob, err_rank, joint, l0, lambda, mu, nu, Q, rank
+  double precision, dimension(:, :), allocatable :: rij
+  double precision :: a, alpha, avg_g, avg_prob, avg_prob2, avg_rank, avg_rank2, &
+    b, err_prob, err_rank, l, lambda, mu, nu, point, Q, rank
 
-  integer, dimension(:), allocatable :: non_susceptible, origins, ranks
-  integer :: c, gsize, i, idx, ins, instances, N, node_i, point, points, &
-    seeds, t0, total
+  integer, dimension(:), allocatable :: ranks
+  integer :: c, gsize, i, instances, N, p, points, seeds, t0
 
-  logical :: restr
+  logical :: dmpr
 
   type(int_llist), dimension(:), allocatable :: indices
 
@@ -35,33 +30,73 @@ program dmp_lambda
 
 
   !Parameters.
-  open(unit=10, file='prm_lambda.txt')
-    read(10, *) restr
-    read(10, *) model
-    read(10, *) seeds, points, instances
-    read(10, *) c, l0, N
-    read(10, *) alpha, mu, nu, t0, Q
+  open(unit=10, file='parameters.txt')
+    read(10, *) dmpr
+    read(10, *) c, l, N
+    read(10, *) graph, model, param
+    read(10, *) a, b, points, scale, instances
+    read(10, *) seeds, alpha, lambda, mu, nu, t0, Q
   close(10)
 
-  !We allocate the main arrays once we have loaded the parameters that tell us
-  !their size.
-  allocate(origins(seeds), ranks(seeds))
-  allocate(tmp_dmp_probs(t0, 4), tmp_mc_probs(t0, 4))
-  allocate(states(N), ps(N), pe(N), pi(N), pr(N), indices(N), history(N), &
-    network(N))
+  !We allocate the array containing the ranks of the seeds.
+  allocate(ranks(seeds))
 
-  !We initialize the random number generator with a fixed seed so that we can
-  !reproduce the same results.
+  !We initialize the random number generator.
   call setr1279(1)
 
-  !We generate the network and apply the rewiring algorithm to it, which creates
-  !the history and indices.
-  network = RRG(N, c)
-  call rewiring('rrg', network, history, indices, c, t0, Q)
+  !Initialization of the node positions.
+  if (trim(graph) == 'PN') then
+    allocate(rij(N, 2))
+    rij = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
+  end if
 
-  !We vary the lambda parameter.
-  do point = 1, points
-    lambda = a + (b-a)*(point-1)/(points-1)
+  do p = 1, points
+    !The chosen parameter will vary differently depending on the selected scale.
+    select case (trim(scale))
+      case ('log')
+        point = exp(log(a) + log(b/a)*(p-1)/(points-1))
+      case default
+        point = a + (b-a)*(p-1)/(points-1)
+    end select
+
+    !We modify the chosen parameter.
+    select case (trim(param))
+      case ('N')
+        N = int(point)
+      case ('l')
+        l = point
+      case ('lambda')
+        lambda = point
+      case ('t0')
+        t0 = int(point)
+    end select
+
+    !If we choose to vary N or l, we have to rebuild the network, history and
+    !indices for each new value of the parameter.
+    if ((trim(param) == 'N').and.allocated(network)) then
+      deallocate(history, indices, network, rij)
+
+      !Additionally, we have to reposition each node if N changes.
+      allocate(rij(N, 2))
+      rij = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
+    else if ((trim(param) == 'l').and.allocated(network)) then
+      deallocate(history, indices, network)
+    end if
+
+    if (.not.allocated(network)) then
+      allocate(history(N), indices(N), network(N))
+
+      !We create a network of the chosen type.
+      select case (trim(graph))
+        case ('PN')
+          network = PN(N, c, rij, l)
+        case ('RRG')
+          network = RRG(N, c)
+      end select
+
+      !We rewire the network to obtain the history and indices.
+      call rewiring(graph, network, history, indices, c, t0, Q)
+    end if
 
     !Initialization of the average quantities.
     avg_g = 0.0d0
@@ -70,90 +105,19 @@ program dmp_lambda
     avg_prob2 = 0.0d0
     avg_rank2 = 0.0d0
 
-    !For each lambda, we run multiple instances.
-    do ins = 1, instances
-      !We neglect those cases where the epidemic does not spread.
-      do while (.true.)
-        !We choose the origins at random.
-        origins = [(1 + floor(N*r1279()), i=1,seeds)]
+    do i = 1, instances
+      !We compute the ranks of the seeds and the size of the epidemic.
+      call pz_sim(model, dmpr, history, indices, seeds, ranks, gsize, alpha, &
+        lambda, mu, nu, t0)
 
-        !We apply a Monte Carlo simulation to spread an infection on the network
-        !using the rules of the S(E)IR model.
-        call mc_sim(model, history, indices, origins, alpha, lambda, mu, nu, &
-          t0, states, 1, tmp_mc_probs)
-
-        !If the epidemic spread, we exit the loop.
-        if (count(states /= 'S') > 1) exit
-      end do
-
-      !We look for nodes that are not S in the network at time t0 because they
-      !are the ones that possibly started the epidemic.
-      allocate(non_susceptible(N))
-      non_susceptible = [(i, i=1,N)]
-      call my_pack(non_susceptible, states/='S')
-      gsize = size(non_susceptible)
-      allocate(energies(gsize))
-
-      !We loop over each of the non-susceptible nodes.
-      do idx = 1, gsize
-        node_i = non_susceptible(idx)
-
-        !We apply the DMP(r) algorithm to calculate the marginal probabilities
-        !that each node is in a state X (i.e., S, (E), I or R) at time t0,
-        !conditional on the fact that node i is the patient zero.
-        call dmp(model, restr, history, indices, states, [node_i], alpha, &
-          lambda, mu, nu, t0, ps, pe, pi, pr, tmp_dmp_probs)
-
-        !The probability that node i is the patient zero given the observation
-        !of the network at time t0 P(i|O) is proportional to P(O|i) (thanks to
-        !Bayes' theorem); we approximate the later joint probability by a
-        !product of the DMP(r) marginals.
-        total = 0
-        joint = 1.0d0
-        do i = 1, N
-          select case (states(i))
-            case ('S')
-              joint = joint * ps(i)
-            case ('E')
-              joint = joint * pe(i)
-            case ('I')
-              joint = joint * pi(i)
-            case ('R')
-              joint = joint * pr(i)
-          end select
-
-          !It may happen that 'joint' becomes so small that GFortran converts its
-          !value to zero, losing all the information it carries. To avoid this,
-          !we multiply it by a huge constant every time it surpasses a certain
-          !threshold. Next, we use the properties of logarithms to compute the
-          !correct energy of a given node.
-          if (abs(joint) < small) then
-            joint = joint / small
-            total = total + 1
-          end if
-        end do
-
-        !Sometimes joint <= 0, so to avoid having to deal with infinities we
-        !associate a large constant energy to the node in question.
-        if (joint > 0.0d0) then
-          energies(idx) = total*log(1/small) - log(joint)
-        else
-          energies(idx) = 1 / small
-        end if
-      end do
-
-      !We rank non-susceptible nodes by their energy in ascending order (0 <= r).
-      call quicksort(energies, non_susceptible)
-      !We compute the ranks of the true seeds.
-      ranks = [(find(non_susceptible, origins(i))-1, i=1,seeds)]
       !For multiple seeds, we use the average of their ranks as a measure of the
       !efficiency of the algorithm.
       rank = sum(ranks) / dble(gsize*seeds)
 
       !We count in how many instances the algorithm predicts that at least one
       !of the original seeds has a rank that falls within the bottom 1% of the
-      !ranks.
-      if (any(ranks <= N/100)) then
+      !ranks. If N is too large, we set the upper limit to 100.
+      if (any(ranks <= min(100, N/100))) then
         avg_prob = avg_prob + 1.0d0
         avg_prob2 = avg_prob2 + 1.0d0
       end if
@@ -162,10 +126,6 @@ program dmp_lambda
       avg_g = avg_g + dble(gsize)
       avg_rank = avg_rank + rank
       avg_rank2 = avg_rank2 + rank*rank
-
-      !We deallocate the list of non-susceptible nodes and their energies for
-      !the next iteration.
-      deallocate(energies, non_susceptible)
     end do
 
     !Normalization of the main magitudes.
@@ -179,6 +139,6 @@ program dmp_lambda
     err_prob = sqrt((avg_prob2-avg_prob**2)/dble(instances-1))
     err_rank = sqrt((avg_rank2-avg_rank**2)/dble(instances-1))
 
-    write(*, '(6es26.16)') lambda, avg_g/N, avg_rank, err_rank, avg_prob, err_prob
+    write(*, '(6es26.16)') point, avg_g/N, avg_rank, err_rank, avg_prob, err_prob
   end do
-end program dmp_lambda
+end program patient_zero
--- simulations/dmp_lambda.mk
+++ simulations/dmp_lambda.mk
@@ -1,5 +1,5 @@
 #Author: Adria Meca Montserrat.
-#Last modified date: 26/05/22.
+#Last modified date: 29/05/22.
 
 #Function that filters the strings that contain the substring 'v' from a given
 #list of strings.
@@ -19,11 +19,13 @@ options := -I $(mod_dir) -J $(mod_dir) -g -Wall -Wextra -pedantic -std=f2003 -O3
 sources := $(call my_filter,properties,$(wildcard $(modules)*.f90))
 #Resulting output files.
 objects := $(patsubst $(modules)%.f90,$(obj_dir)%.o,$(sources))
+
 #List of commonly used 'o' files.
-my_list := $(filter-out %procedures.o %generator.o,$(objects))
+in_list := $(filter $(obj_dir)dmp% $(obj_dir)mc%,$(objects))
+out_list := $(filter-out %procedures.o %generator.o,$(objects))
 
 #Main program and its byproducts.
-src := ./dmp_lambda.f90
+src := ./patient_zero.f90
 obj := $(patsubst %.f90,%.o,$(src))
 exe := $(patsubst %.o,%.exe,$(obj))
 
@@ -36,9 +38,10 @@ $(objects): $(obj_dir)%.o: $(modules)%.f90
 	gfortran $(options) -c $< -o $@
 
 #Module interdependencies.
-$(my_list): $(obj_dir)array_procedures.o
-$(call my_filter,dmp,$(my_list)): $(obj_dir)random_number_generator.o
-$(call my_filter,generation,$(my_list)): $(obj_dir)network_generation.o
+$(out_list): $(obj_dir)array_procedures.o
+$(filter %problem.o,$(out_list)): $(in_list)
+$(call my_filter,dmp,$(out_list)): $(obj_dir)random_number_generator.o
+$(call my_filter,generation,$(out_list)): $(obj_dir)network_generation.o
 
 #Command that cleans up the byproducts of the main program's compilation.
 clean:
