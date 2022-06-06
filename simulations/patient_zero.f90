e00f521b28f124544937f4cf26fadc39e87c32fa
DrMeca <amecamon7@gmail.com>
2022-06-06 15:49:01 +0200

    Several improvements: 1) we change the random sequence for each point; 2) we generate a random graph for each instance; 3) N is no longer a parameter; 4) some simplifications.

--- simulations/patient_zero.f90
+++ simulations/patient_zero.f90
@@ -2,12 +2,12 @@
 !the precision with which it locates the true patient zero(s) in a network at a
 !certain time t as we vary one of the parameters, leaving all the others fixed.
 !Author: Adria Meca Montserrat.
-!Last modified date: 31/05/22.
+!Last modified date: 06/06/22.
 program patient_zero
   use array_procedures, only : int_llist
   use network_generation, only : node, PN, RRG
   use patient_zero_problem, only : pz_sim
-  use random_number_generator, only : r1279, setr1279
+  use random_number_generator, only : ir1279, r1279, setr1279
   use rewiring_algorithms, only : rewiring
 
   implicit none
@@ -15,16 +15,17 @@ program patient_zero
   !Variables.
   character(len=9) :: graph, model, param, scale
 
-  double precision, dimension(:, :), allocatable :: rij
+  double precision, dimension(:, :), allocatable :: r
   double precision, dimension(3) :: avg_g
-  double precision :: a, alpha, avg_prob, avg_prob2, avg_rank, avg_rank2, b, &
-    err_prob, err_rank, l, lambda, mu, nu, point, Q, rank
+  double precision :: a, alpha, avg_prob, avg_rank, avg_rank2, b, err_rank, l, &
+    lambda, mu, nu, point, Q, rank
 
   integer, dimension(:), allocatable :: ranks
+  integer, dimension(8) :: values
   integer, dimension(3) :: gsize
-  integer :: c, i, instances, N, p, points, seeds, t0
+  integer :: c, i, idx, instances, iseed, N, p, points, seeds, t0
 
-  logical :: dmpr
+  logical :: dmpr, rng
 
   type(int_llist), dimension(:), allocatable :: indices
 
@@ -33,6 +34,7 @@ program patient_zero
 
   !Parameters.
   open(unit=10, file='parameters.txt')
+    read(10, *) rng
     read(10, *) dmpr
     read(10, *) c, l, N
     read(10, *) graph, model, param
@@ -40,19 +42,24 @@ program patient_zero
     read(10, *) seeds, alpha, lambda, mu, nu, t0, Q
   close(10)
 
-  !We allocate the array containing the ranks of the seeds.
-  allocate(ranks(seeds))
+  allocate(history(N), indices(N), network(N), r(N, 2), ranks(seeds))
 
   !We initialize the random number generator.
-  call setr1279(1)
+  if (rng) then
+    call date_and_time(values=values)
+    iseed = sum(values)
+  else
+    iseed = 0
+  end if
+  call setr1279(iseed)
 
   !Initialization of the node positions.
-  if (trim(graph) == 'PN') then
-    allocate(rij(N, 2))
-    rij = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
-  end if
+  r = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
 
   do p = 1, points
+    !We restart the random sequence.
+    call setr1279(ir1279())
+
     !The chosen parameter will vary differently depending on the selected scale.
     select case (trim(scale))
       case ('log')
@@ -63,8 +70,6 @@ program patient_zero
 
     !We modify the chosen parameter.
     select case (trim(param))
-      case ('N')
-        N = int(point)
       case ('l')
         l = point
       case ('lambda')
@@ -73,47 +78,29 @@ program patient_zero
         t0 = int(point)
     end select
 
-    !If we choose to vary N or l, we have to rebuild the network, history and
-    !indices for each new value of the parameter.
-    if ((trim(param) == 'N').and.allocated(network)) then
-      deallocate(history, indices, network, rij)
-
-      !Additionally, we have to reposition each node if N changes.
-      allocate(rij(N, 2))
-      rij = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
-    else if ((trim(param) == 'l').and.allocated(network)) then
-      deallocate(history, indices, network)
-    end if
-
-    if (.not.allocated(network)) then
-      allocate(history(N), indices(N), network(N))
-
-      !We create a network of the chosen type.
-      select case (trim(graph))
-        case ('PN')
-          network = PN(N, c, rij, l)
-        case ('RRG')
-          network = RRG(N, c)
-      end select
-
-      !We rewire the network to obtain the history and indices.
-      call rewiring(graph, network, history, indices, c, t0, Q)
-    end if
-
     !Initialization of the average quantities.
     avg_g = 0.0d0
     avg_prob = 0.0d0
     avg_rank = 0.0d0
-    avg_prob2 = 0.0d0
     avg_rank2 = 0.0d0
 
-    do i = 1, instances
+    do idx = 1, instances
+      !We create a network of the chosen type.
+      select case (trim(graph))
+        case ('PN')
+          network = PN(N, c, r, l)
+        case ('RRG')
+          network = RRG(N, c)
+      end select
+
+      !We rewire the connections of the network.
+      call rewiring(graph, network, history, indices, c, t0, r, l, Q)
+
       !We compute the ranks of the seeds and the size of the epidemic.
       call pz_sim(model, dmpr, history, indices, seeds, ranks, gsize, alpha, &
         lambda, mu, nu, t0)
 
-      !For multiple seeds, we use the average of their ranks as a measure of the
-      !efficiency of the algorithm.
+      !We use the average of the rank of the seeds as a measure of performance.
       rank = sum(ranks) / dble(sum(gsize)*seeds)
 
       !We count in how many instances the algorithm predicts that at least one
@@ -121,7 +108,6 @@ program patient_zero
       !ranks. If N is too large, we set the upper limit to 100.
       if (any(ranks <= min(100, N/100))) then
         avg_prob = avg_prob + 1.0d0
-        avg_prob2 = avg_prob2 + 1.0d0
       end if
 
       !We accumulate the quantities of interest.
@@ -134,13 +120,11 @@ program patient_zero
     avg_g = avg_g / instances
     avg_prob = avg_prob / instances
     avg_rank = avg_rank / instances
-    avg_prob2 = avg_prob2 / instances
     avg_rank2 = avg_rank2 / instances
 
-    !Uncertainties of the main quantities.
-    err_prob = sqrt((avg_prob2-avg_prob**2)/dble(instances-1))
+    !Uncertainty associated with the rank.
     err_rank = sqrt((avg_rank2-avg_rank**2)/dble(instances-1))
 
-    write(*, '(9es26.16)') point, avg_g/N, avg_rank, err_rank, avg_prob, err_prob
+    write(*, '(7es26.16)') point, avg_g/N, avg_rank, err_rank, avg_prob
   end do
 end program patient_zero
