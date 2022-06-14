b0cd712729ef8c8d00d55535a829be7db3cd5e9c
DrMeca <amecamon7@gmail.com>
2022-06-14 23:32:15 +0200

    New simulation added: trajectories.

---
+++ simulations/trajectories.f90
@@ -0,0 +1,145 @@
+!Simulation that compares the trajectories given by DMP and MC.
+!Author: Adria Meca Montserrat.
+!Last modified date: 14/06/22.
+program trajectories
+  use array_procedures, only : int_llist
+  use dmp_algorithms, only : dmp
+  use mc_simulations, only : mc_sim
+  use network_generation, only : node, PN, RRG
+  use random_number_generator, only : ir1279, r1279, setr1279
+  use rewiring_algorithms, only : rewiring
+
+  implicit none
+
+  !Variables.
+  character(len=1), dimension(:), allocatable :: states
+  character(len=9) :: graph, mode, model
+
+  double precision, dimension(:, :), allocatable :: avg_dmp, avg_mc, grid, r, &
+    tmp_dmp, tmp_mc
+  double precision, dimension(:), allocatable :: pe, pi, pr, ps
+  double precision :: alpha, l, lambda, mu, nu, Q
+
+  integer, dimension(:), allocatable :: origins
+  integer, dimension(8) :: values
+  integer :: c, i, idx, instances, iseed, M, N, points, s, seeds, t, t0, x, y
+
+  logical :: dmpr, rng
+
+  type(int_llist), dimension(:), allocatable :: indices
+
+  type(node), dimension(:), allocatable :: history, network
+
+
+  !Parameters.
+  open(unit=10, file='trajectories.txt')
+    read(10, *) rng
+    read(10, *) dmpr
+    read(10, *) c, l, N
+    read(10, *) graph, mode, model
+    read(10, *) M, s, points, instances
+    read(10, *) seeds, alpha, lambda, mu, nu, t0, Q
+  close(10)
+
+  allocate(avg_dmp(t0, 4), avg_mc(t0, 4), grid(points, points), r(N, 2), &
+    tmp_dmp(t0, 4), tmp_mc(t0, 4))
+
+  allocate(history(N), indices(N), network(N), origins(seeds), states(N), &
+    pe(N), pi(N), pr(N), ps(N))
+
+  !We initialize the random number generator.
+  if (rng) then
+    call date_and_time(values=values)
+    iseed = sum(values)
+  else
+    iseed = 0
+  end if
+  call setr1279(iseed)
+
+  !Initialization of the average quantities.
+  grid = 0.0d0
+  avg_mc = 0.0d0
+  avg_dmp = 0.0d0
+
+  do idx = 1, instances
+    !We restart the random sequence.
+    call setr1279(ir1279())
+
+    !Initialization of the node positions.
+    r = sqrt(dble(N)) * reshape([(r1279(), i=1,2*N)], [N, 2])
+
+    !We choose the origins at random.
+    origins = [(1 + mod(int(N*r1279()), N), i=1,seeds)]
+
+    !We create a network of the chosen type.
+    select case (trim(graph))
+      case ('PN')
+        network = PN(N, c, r, l)
+      case ('RRG')
+        network = RRG(N, c)
+    end select
+
+    !We rewire the connections of the network.
+    call rewiring(graph, network, history, indices, c, t0, r, l, Q)
+
+    !We compare the DMP and MC trajectories for a given pair (mu, lambda).
+    if (trim(mode) == 'line') then
+      !DMP.
+      call dmp(model, dmpr, history, indices, states, origins, alpha, &
+        lambda, mu, nu, t0, ps, pe, pi, pr, tmp_dmp)
+
+      !MC.
+      call mc_sim(model, history, indices, origins, alpha, lambda, mu, nu, &
+        t0, states, M, tmp_mc)
+
+      avg_mc = avg_mc + tmp_mc
+      avg_dmp = avg_dmp + tmp_dmp
+    !We compute the square root of the difference squared between the DMP and MC
+    !trajectories for each point (mu, lambda) in a given grid.
+    else if (trim(mode) == 'grid') then
+      do y = 1, points
+        lambda = (y-1) / dble(points-1)
+        do x = 1, points
+          mu = (x-1) / dble(points-1)
+
+          !DMP.
+          call dmp(model, dmpr, history, indices, states, origins, alpha, &
+            lambda, mu, nu, t0, ps, pe, pi, pr, tmp_dmp)
+
+          !MC.
+          call mc_sim(model, history, indices, origins, alpha, lambda, mu, nu, &
+            t0, states, M, tmp_mc)
+
+          grid(x, y) = grid(x, y) + sum(sqrt((tmp_dmp(:, s)-tmp_mc(:, s))**2))/t0
+        end do
+      end do
+    end if
+  end do
+
+  !Normalization of the main magnitudes.
+  grid = grid / instances
+  avg_mc = avg_mc / instances
+  avg_dmp = avg_dmp / instances
+
+  select case (trim(mode))
+    case ('line')
+      !Headers.
+      write(*, '(a,a25,2a78)') '#', 't', 'S, I, R (MC)', 'S, I, R (DMP)'
+
+      do t = 1, t0
+        write(*, '(i26,6es26.16)') t, avg_mc(t, [1, 3, 4]), avg_dmp(t, [1, 3, 4])
+      end do
+    case ('grid')
+      !Headers.
+      write(*, '(a,a25,2a26)') '#', 'mu', 'lambda', 'SRDS'
+
+      do y = 1, points
+        lambda = (y-1) / dble(points-1)
+        do x = 1, points
+          mu = (x-1) / dble(points-1)
+          write(*, '(3es26.16)') mu, lambda, grid(x, y)
+        end do
+        print*
+      end do
+  end select
+end program trajectories
---
+++ simulations/trajectories.mk
@@ -0,0 +1,57 @@
+#Author: Adria Meca Montserrat.
+#Last modified date: 14/06/22.
+
+#Function that filters the strings that contain the substring 'v' from a given
+#list of strings.
+my_filter = $(foreach v,$(2),$(if $(findstring $(1),$(v)),,$(v)))
+
+#Folder containing the modules whose procedures are used in the main simulations.
+modules := ../modules/
+#Folder containing the mod files that result from the compilation of the modules.
+mod_dir := ../mod_dir/
+#Folder containing the 'o' files that result from the compilation of the modules.
+obj_dir := ../obj_dir/
+
+#Compilation flags.
+options := -I $(mod_dir) -J $(mod_dir) -g -Wall -Wextra -pedantic -std=f2003 -O3
+
+#Modules of interest.
+sources := $(call my_filter,properties,$(wildcard $(modules)*.f90))
+sources := $(call my_filter,zero,$(sources))
+#Resulting output files.
+objects := $(patsubst $(modules)%.f90,$(obj_dir)%.o,$(sources))
+
+#List of commonly used 'o' files.
+in_list := $(filter $(obj_dir)dmp% $(obj_dir)mc%,$(objects))
+out_list := $(filter-out %procedures.o %generator.o,$(objects))
+
+#Main program and its byproducts.
+src := ./trajectories.f90
+obj := $(patsubst %.f90,%.o,$(src))
+exe := $(patsubst %.o,%.exe,$(obj))
+
+#Compilation instructions.
+$(exe): $(obj)
+	gfortran $(objects) $< -o $@
+$(obj): $(src) $(objects)
+	gfortran $(options) -c $< -o $@
+$(objects): $(obj_dir)%.o: $(modules)%.f90
+	gfortran $(options) -c $< -o $@
+
+#Module interdependencies.
+$(out_list): $(obj_dir)array_procedures.o
+$(filter %problem.o,$(out_list)): $(in_list)
+$(call my_filter,dmp,$(out_list)): $(obj_dir)random_number_generator.o
+$(call my_filter,generation,$(out_list)): $(obj_dir)network_generation.o
+
+#Command that cleans up the byproducts of the main program's compilation.
+clean:
+	rm -f $(exe) $(obj)
+
+#Command that removes all the files that result from the compilation process.
+hard-clean:
+	rm -f $(mod_dir)*.mod $(obj_dir)*.o $(exe) $(obj)
+
+#Command that runs the executable of the main program.
+run:
+	@$(exe)
