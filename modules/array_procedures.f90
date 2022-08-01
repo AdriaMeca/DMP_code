c7b469c7b26835799ab7375ba859078375922965
DrMeca <amecamon7@gmail.com>
2022-08-01 17:12:04 +0200

    I've changed the style and improved the comments.

--- modules/array_procedures.f90
+++ modules/array_procedures.f90
@@ -1,63 +1,51 @@
-!Module whose procedures create, modify and study arrays.
-!Author: Adria Meca Montserrat.
-!Last modified date: 05/06/22.
+!> Procedures to create, modify and study arrays.
+!> Author: Adria Meca Montserrat.
+!> Last modified date: 01/08/22.
 module array_procedures
   implicit none
 
   private
 
-  !List of doubles.
   type, public :: dbl_list
-    double precision, dimension(:), allocatable :: array
+    double precision, allocatable :: array(:)
   end type dbl_list
 
-  !List of integers.
   type, public :: int_list
-    integer, dimension(:), allocatable :: array
+    integer, allocatable :: array(:)
   end type int_list
 
-  !List of lists of integers.
+  !> List of lists of integers.
   type, public :: int_llist
-    type(int_list), dimension(:), allocatable :: time
+    type(int_list), allocatable :: time(:)
   end type int_llist
 
-  !Interface that adds elements to lists.
   interface add
     module procedure add_dbl, add_int, add_int_list
   end interface add
 
-  !Interface that searches for elements in lists.
   interface find
     module procedure find_int
   end interface find
 
-  !Interface that returns the indices that sort a given list in ascending order.
   interface idx_insertion_sort
     module procedure dbl_idx_insertion_sort, int_idx_insertion_sort
   end interface idx_insertion_sort
 
-  !Interface that filters different lists given a condition.
   interface my_pack
     module procedure my_pack_int
   end interface my_pack
 
-  public add, find, idx_insertion_sort, my_pack, pop, quicksort
+  public :: add, find, idx_insertion_sort, my_pack, pop, quicksort
 
 contains
-  !Function that searches the index of an integer in a list of integers.
+  !> Looks for the position of an integer in a list of integers.
   function find_int(list, element)
-    implicit none
-
-    !Input arguments.
-    integer, dimension(:), intent(in) :: list
+    integer, intent(in) :: list(:)
     integer, intent(in) :: element
 
-    !Output arguments.
-    integer :: find_int
+    integer             :: find_int  !> Output.
 
-    !Local variables.
-    integer :: i
-
+    integer             :: i
 
     find_int = 0
     do i = 1, size(list)
@@ -69,23 +57,14 @@ contains
   end function find_int
 
 
-
-  !Function that returns the permutation of the indices that orders its
-  !associated list of doubles.
+  !> Returns the indices that sort a list of doubles in ascending order.
   function dbl_idx_insertion_sort(list)
-    implicit none
+    double precision, intent(in) :: list(:)
 
-    !Input arguments.
-    double precision, dimension(:), intent(in) :: list
+    integer                      :: dbl_idx_insertion_sort(size(list))  !> Output.
 
-    !Output arguments.
-    integer, dimension(size(list)) :: dbl_idx_insertion_sort
-
-    !Local variables.
-    double precision, dimension(size(list)) :: copy_list
-
-    integer :: i, j
-
+    double precision             :: copy_list(size(list))
+    integer                      :: i, j
 
     copy_list = list
     dbl_idx_insertion_sort = [(i, i=1,size(list))]
@@ -101,22 +80,14 @@ contains
   end function dbl_idx_insertion_sort
 
 
-
-  !Function that returns the permutation of the indices that orders its
-  !associated list of integers.
+  !> Returns the indices that sort a list of integers in ascending order.
   function int_idx_insertion_sort(list)
-    implicit none
+    integer, intent(in) :: list(:)
 
-    !Input arguments.
-    integer, dimension(:), intent(in) :: list
+    integer             :: int_idx_insertion_sort(size(list))  !> Output.
 
-    !Output arguments.
-    integer, dimension(size(list)) :: int_idx_insertion_sort
-
-    !Local variables.
-    integer, dimension(size(list)) :: copy_list
-    integer :: i, j
-
+    integer             :: copy_list(size(list))
+    integer             :: i, j
 
     copy_list = list
     int_idx_insertion_sort = [(i, i=1,size(list))]
@@ -132,27 +103,19 @@ contains
   end function int_idx_insertion_sort
 
 
+  !> Returns a list without its i-th element.
+  function pop(list, i)
+    double precision,              intent(in) :: list(:)
+    integer,                       intent(in) :: i
 
-  !Function that returns a list without the element placed at index.
-  function pop(list, index)
-    implicit none
+    double precision, allocatable             :: pop(:)   !> Output.
 
-    !Input arguments.
-    double precision, dimension(:), intent(in) :: list
-
-    integer, intent(in) :: index
-
-    !Output arguments.
-    double precision, dimension(:), allocatable :: pop
-
-    !Local variables.
-    integer :: n
-
+    integer                                   :: n
 
     n = size(list)
-    if ((1 <= index).and.(index <= n)) then
+    if ((1 <= i) .and. (i <= n)) then
       allocate(pop(n-1))
-      pop = [list(:index-1), list(index+1:)]
+      pop = [list(:i-1), list(i+1:)]
     else
       allocate(pop(n))
       pop = list
@@ -160,163 +123,114 @@ contains
   end function pop
 
 
-
-  !Subroutine that adds a double to an array of doubles.
+  !> Adds a double to an array of doubles.
   subroutine add_dbl(list, element)
-    implicit none
+    double precision,              intent(in)    :: element
 
-    !Input/output arguments.
-    double precision, dimension(:), allocatable, intent(inout) :: list
-    double precision, intent(in) :: element
+    double precision, allocatable, intent(inout) :: list(:)
 
-    !Local variables.
-    double precision, dimension(:), allocatable :: copy_list
-
-    integer :: isize
-
+    double precision, allocatable                :: copy_list(:)
+    integer                                      :: isize
 
     if (allocated(list)) then
-      !We create a temporary list with one more element than the original.
+      !> We make a copy of the original list, adding the new element to it.
       isize = size(list)
       allocate(copy_list(1+isize))
-
-      !We copy the elements from the old list to the new one and add the
-      !new element.
       copy_list(1:isize) = list
       copy_list(1+isize) = element
 
-      !We deallocate the original list, make it bigger and transfer all
-      !the elements to it, including the new one. Then, we deallocate
-      !the temporary list because we do not need it anymore.
-      deallocate(list)
-      allocate(list(1+isize))
-      list = copy_list
-      deallocate(copy_list)
+      !> We move the elements of the copied list to the original one.
+      call move_alloc(copy_list, list)
     else
-      !If the original list has no elements, we pass the new item to it.
+      !> If the original list has no elements, we add the new element to it.
       allocate(list(1))
       list(1) = element
     end if
   end subroutine add_dbl
 
 
-
-  !Subroutine that adds an integer to an array of integers.
+  !> Adds an integer to an array of integers.
   subroutine add_int(list, element)
-    implicit none
+    integer,              intent(in)    :: element
 
-    !Input/output arguments.
-    integer, dimension(:), allocatable, intent(inout) :: list
-    integer, intent(in) :: element
+    integer, allocatable, intent(inout) :: list(:)
 
-    !Local variables.
-    integer, dimension(:), allocatable :: copy_list
-    integer :: isize
-
+    integer, allocatable                :: copy_list(:)
+    integer                             :: isize
 
     if (allocated(list)) then
-      !We create a temporary list with one more element than the original.
+      !> We make a copy of the original list, adding the new element to it.
       isize = size(list)
       allocate(copy_list(1+isize))
-
-      !We copy the elements from the old list to the new one and add the
-      !new element.
       copy_list(1:isize) = list
       copy_list(1+isize) = element
 
-      !We deallocate the original list, make it bigger and transfer all
-      !the elements to it, including the new one. Then, we deallocate
-      !the temporary list because we do not need it anymore.
-      deallocate(list)
-      allocate(list(1+isize))
-      list = copy_list
-      deallocate(copy_list)
+      !> We move the elements of the copied list to the original one.
+      call move_alloc(copy_list, list)
     else
-      !If the original list has no elements, we pass the new item to it.
+      !> If the original list has no elements, we add the new element to it.
       allocate(list(1))
       list(1) = element
     end if
   end subroutine add_int
 
 
-
-  !Subroutine that adds a list of integers to a list of lists (of integers).
+  !> Adds a list of integers to a list of lists (of integers).
   subroutine add_int_list(list, element)
-    implicit none
+    type(int_list),              intent(in)    :: element
 
-    !Input/output arguments.
-    type(int_list), dimension(:), allocatable, intent(inout) :: list
-    type(int_list), intent(in) :: element
+    type(int_list), allocatable, intent(inout) :: list(:)
 
-    !Local variables.
-    integer :: isize
-
-    type(int_list), dimension(:), allocatable :: copy_list
-
+    integer                                    :: isize
+    type(int_list), allocatable                :: copy_list(:)
 
     if (allocated(list)) then
-      !We create a temporary list with one more element than the original.
+      !> We make a copy of the original list, adding the new element to it.
       isize = size(list)
       allocate(copy_list(1+isize))
-
-      !We copy the elements from the old list to the new one and add the
-      !new element.
       copy_list(1:isize) = list
       copy_list(1+isize) = element
 
-      !We deallocate the original list, make it bigger and transfer all
-      !the elements to it, including the new one. Then, we deallocate
-      !the temporary list because we do not need it anymore.
-      deallocate(list)
-      allocate(list(1+isize))
-      list = copy_list
-      deallocate(copy_list)
+      !> We move the elements of the copied list to the original one.
+      call move_alloc(copy_list, list)
     else
-      !If the original list has no elements, we pass the new item to it.
+      !> If the original list has no elements, we add the new element to it.
       allocate(list(1))
       list(1) = element
     end if
   end subroutine add_int_list
 
 
-
-  !Custom version of the PACK function provided by GFortran that applies to
-  !lists of integers.
+  !> Custom version of GFortran's PACK function that applies to lists of integers.
   subroutine my_pack_int(list, condition)
-    implicit none
+    logical,              intent(in)    :: condition(:)
 
-    !Input/output arguments.
-    integer, dimension(:), allocatable, intent(inout) :: list
+    integer, allocatable, intent(inout) :: list(:)
 
-    logical, dimension(:), intent(in) :: condition
+    integer, allocatable                :: copy_list(:)
+    integer                             :: i, j, new_size, old_size
 
-    !Local variables.
-    integer, dimension(:), allocatable :: copy_list
-    integer :: i, j, new_size, old_size
-
-
-    if (.not.allocated(list)) then
-      print*, 'An attempt was made to filter an unallocated list.'
+    if (.not. allocated(list)) then
+      print *, 'An attempt was made to filter an unallocated list.'
       return
     end if
 
-    !We transfer all elements from list to copy_list.
+    !> We make a copy of the original list.
     old_size = size(list)
     allocate(copy_list(old_size))
     copy_list = list
 
-    !We compute the new size of the list based on the number of true elements
-    !in the boolean array called condition.
-    new_size = count(condition)
+    !> The new size of the original list is equal to the number of elements that
+    !> meet the condition.
+    new_size = count(condition == .true.)
     deallocate(list)
     allocate(list(new_size))
 
-    !If the new size is greater than zero, we pass all the elements that meet
-    !the condition to our list.
+    !> We construct the new list.
     if (new_size > 0) then
       j = 1
       do i = 1, old_size
-        if (condition(i)) then
+        if (condition(i) == .true.) then
           list(j) = copy_list(i)
           j = j + 1
         end if
@@ -325,40 +239,29 @@ contains
   end subroutine my_pack_int
 
 
-
-  !Recursive subroutine that sorts two lists in ascending order from the values
-  !of the first.
+  !> Sorts two lists by increasing value of the former.
   recursive subroutine quicksort(energies, node_ids)
-    implicit none
+    double precision,              intent(inout) :: energies(:)
+    integer,                       intent(inout) :: node_ids(:)
 
-    !Input/output arguments.
-    double precision, dimension(:), intent(inout) :: energies
+    double precision, allocatable                :: e_eq(:), e_gt(:), e_lt(:)
+    double precision, parameter                  :: eps = 1.0d-12
+    double precision                             :: e, pivot
+    integer,          allocatable                :: n_eq(:), n_gt(:), n_lt(:)
+    integer                                      :: i, isize, n
 
-    integer, dimension(:), intent(inout) :: node_ids
-
-    !Local variables.
-    double precision, dimension(:), allocatable :: e_eq, e_gt, e_lt
-    double precision, parameter :: eps=1.0d-12
-    double precision :: e, pivot
-
-    integer, dimension(:), allocatable :: n_eq, n_gt, n_lt
-    integer :: i, isize, n
-
-
-    !If the size of the list of energies is less than or equal to 1 we do not
-    !have to do anything to it because it is already sorted.
+    !> If the list of energies has less than two elements, it is already sorted.
     isize = size(energies)
     if (isize > 1) then
-      !We choose a pivot arbitrarily.
+      !> We arbitrarily choose a pivot.
       pivot = energies(1)
 
-      !We go through all the elements of the list of energies, classifying
-      !them according to whether they are equal to, less than or greater
-      !than the pivot; we classify node identifiers in the same way.
+      !> We classify the energies according to whether they are equal to, less than
+      !> or greater than the pivot. Nodes are classified in the same way.
       do i = 1, isize
         e = energies(i)
         n = node_ids(i)
-        if (abs(e-pivot) < eps) then
+        if (abs(e - pivot) < eps) then
           call add(e_eq, e)
           call add(n_eq, n)
         else if (e < pivot) then
@@ -370,17 +273,17 @@ contains
         end if
       end do
 
-      !If the list containing the elements that are greater (smaller) than the
-      !pivot has not been allocated, we allocate it to avoid memory errors.
-      if (.not.allocated(e_lt)) allocate(e_lt(0), n_lt(0))
-      if (.not.allocated(e_gt)) allocate(e_gt(0), n_gt(0))
+      !> We make sure that the lists containing the elements that are smaller and
+      !> greater than the pivot have been allocated to avoid memory errors.
+      if (.not. allocated(e_lt)) allocate(e_lt(0), n_lt(0))
+      if (.not. allocated(e_gt)) allocate(e_gt(0), n_gt(0))
 
-      !We apply the subroutine recursively to the lists that contain the elements
-      !that are smaller and greater than the pivot, respectively.
+      !> We recursively apply the subroutine to the lists containing the elements
+      !> that are smaller and greater than the pivot.
       call quicksort(e_lt, n_lt)
       call quicksort(e_gt, n_gt)
 
-      !Finally, we construct the sorted lists.
+      !> Finally, we construct the sorted lists.
       energies = [e_lt, e_eq, e_gt]
       node_ids = [n_lt, n_eq, n_gt]
     end if
