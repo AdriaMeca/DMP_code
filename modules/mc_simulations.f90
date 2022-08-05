70635b778a03bc5dbb85d2c6303ddbeb3d48e08d
DrMeca <amecamon7@gmail.com>
2022-08-05 15:56:20 +0200

    I've changed the style, improved the comments and made the changes requested by Matteo.

--- modules/mc_simulations.f90
+++ modules/mc_simulations.f90
@@ -1,107 +1,104 @@
-!Module that contains procedures that simulate the spread of epidemics on
-!dynamic networks using Monte Carlo algorithms that impose different
-!epidemiological models (SIR and SEIR).
-!Author: Adria Meca Montserrat.
-!Last modified date: 17/06/22.
+!> Procedures that simulate the spread of epidemics on time-varying networks.
+!> Author: Adria Meca Montserrat.
+!> Last modified date: 05/08/22.
 module mc_simulations
-  use array_procedures, only : int_llist
-  use network_generation, only : node
-  use random_number_generator, only : r1279
+  use array_procedures, only: int_llist
+  use network_generation, only: node
+  use random_number_generator, only: r1279
 
   implicit none
 
   private
 
-  public mc_sim
+  public :: mc_sim
 
 contains
-  !Subroutine that spreads an infection that follows the S(E)IR rules on a network
-  !using a Monte Carlo simulation.
-  subroutine mc_sim(model, history, indices, origins, alpha, lambda, mu, nu, t0, &
-    states, realizations, tmp_mc_probs)
-    implicit none
+  !> Spreads an S(E)IR epidemic on a time-varying network using Monte Carlo.
+  subroutine mc_sim( &
+    model,           &
+    history,         &
+    indices,         &
+    origins,         &
+    alpha_,          &
+    lambda_,         &
+    mu_,             &
+    nu_,             &
+    t0,              &
+    tmp_states,      &
+    repetitions,     &
+    tmp_mc_probs     &
+  )
+    character(len=*),              intent(in)  :: model                      !> Epidemiological model.
+    character(len=1),              intent(out) :: tmp_states(0:, :)          !> Node states.
+    character(len=1)                           :: auxsts(size(history))      !>
+    character(len=1)                           :: states(size(history))      !>
+    double precision,              intent(in)  :: alpha_, lambda_, mu_, nu_  !>
+    double precision,              intent(out) :: tmp_mc_probs(:, :)         !> Temporal MC probabilities.
+    double precision                           :: alpha, lambda, mu, nu      !> Epidemiological parameters.
+    double precision                           :: p1, p2, r                  !>
+    integer,                       intent(in)  :: origins(:)                 !> Patient zeros.
+    integer,                       intent(in)  :: repetitions                !>
+    integer,                       intent(in)  :: t0                         !> Observation time.
+    integer,          allocatable              :: nbrs(:)                    !>
+    integer                                    :: alti, i, isize, N, t       !>
+    type(int_llist),               intent(in)  :: indices(:)                 !> Active links throughout the simulation.
+    type(node),                    intent(in)  :: history(:)                 !> Rewiring history.
 
-    !Input arguments.
-    character(len=*), intent(in) :: model
-
-    double precision, intent(in) :: alpha, lambda, mu, nu
-
-    integer, dimension(:), intent(in) :: origins
-    integer, intent(in) :: realizations, t0
-
-    type(int_llist), dimension(:), intent(in) :: indices
-
-    type(node), dimension(:), intent(in) :: history
-
-    !Output arguments.
-    character(len=1), dimension(:), intent(out) :: states
-
-    double precision, dimension(:, :), intent(out) :: tmp_mc_probs
-
-    !Local variables.
-    character(len=1), dimension(size(history)) :: auxsts
-
-    double precision :: altalpha, altlambda, altmu, altnu, p1, p2, r
-
-    integer, dimension(:), allocatable :: nbrs
-    integer :: alti, i, isize, N, t
-
-
-    !Number of nodes.
+    !> Number of nodes.
     N = size(history)
 
-    !We choose the local epidemiological parameters based on the model in use.
+    !> We choose the local epidemiological parameters based on the model in use.
     if (trim(model) == 'SIR') then
-      altnu = mu
-      altmu = 0.0d0
-      altalpha = lambda
-      altlambda = 0.0d0
+      nu = mu_
+      mu = 0.0d0
+      alpha = lambda_
+      lambda = 0.0d0
     else
-      altmu = mu
-      altnu = nu
-      altalpha = alpha
-      altlambda = lambda
+      mu = mu_
+      nu = nu_
+      alpha = alpha_
+      lambda = lambda_
     end if
 
     tmp_mc_probs = 0.0d0
-    do alti = 1, realizations
-      !We initialize the array of states.
+    do alti = 1, repetitions
+      !> We initialize the distribution of node states.
       states = 'S'
       states(origins) = 'E'
-      !We initialize an auxiliary array of states.
+
+      !> We initialize an auxiliary array of states.
       auxsts = states
 
+      tmp_states(0, :) = states
+
+      !> We let the system evolve by simulating the S(E)IR rules with MC.
       do t = 1, t0
-        !We apply the MC core to modify the states using the S(E)IR rules.
         do i = 1, N
-          !We compute the neighbors of node i at time t.
+          !> We retrieve the neighbors of node i at time t.
           isize = size(indices(i)%time(t)%array)
           allocate(nbrs(isize))
           nbrs = history(i)%neighbors(indices(i)%time(t)%array)
 
-          !We apply the S(E)IR rules at each t: an E (exposed) node becomes I with
-          !probability nu, and an I node becomes R with probability mu, regardless
-          !of their respective neighbors; a S node becomes E with probability alpha
-          !when it makes contact with one of its E neighbors (modified SEIR); also,
-          !it becomes E with probability lambda when it interacts with one of its I
-          !neighbors.
           r = r1279()
-          if ((auxsts(i) == 'E').and.(r < altnu)) then
+          if ((auxsts(i) == 'E') .and. (r < nu)) then
             states(i) = 'I'
-          else if ((auxsts(i) == 'I').and.(r < altmu)) then
+          else if ((auxsts(i) == 'I') .and. (r < mu)) then
             states(i) = 'R'
           else if (auxsts(i) == 'S') then
-            p1 = 1.0d0 - product(1.0d0-altalpha*merge(1, 0, auxsts(nbrs)=='E'))
-            p2 = 1.0d0 - product(1.0d0-altlambda*merge(1, 0, auxsts(nbrs)=='I'))
+            p1 = 1.0d0 - product(1.0d0-alpha*merge(1, 0, auxsts(nbrs) == 'E'))
+            p2 = 1.0d0 - product(1.0d0-lambda*merge(1, 0, auxsts(nbrs) == 'I'))
             if (r < p1+p2) states(i) = 'E'
           end if
 
           deallocate(nbrs)
         end do
-        !We update auxsts at the end of each time step.
+
+        !> We update auxsts at the end of each time step.
         auxsts = states
 
-        !We count the number of nodes that are in each state at time t.
+        tmp_states(t, :) = states
+
+        !> We count the number of nodes that are in each state at time t.
         do i = 1, N
           select case (states(i))
             case ('S')
@@ -116,24 +113,25 @@ contains
         end do
       end do
     end do
-    !We divide the previous quantities by N * realizations, thus obtaining the
-    !average trajectory of the fraction of nodes that are in state X (i.e., S,
-    !E, I or R).
-    tmp_mc_probs = tmp_mc_probs / N / realizations
 
-    !If we are considering the SIR model, in the end we have to perform the
-    !following exchanges: (PE, PI, PR) --> (0.0, PE, PI) and ('E', 'I') -->
-    !('I', 'R').
+    !> We divide the above quantities by N * repetitions to obtain the trajectory
+    !> of the fraction of nodes in state X (i.e., S, E, I or R).
+    tmp_mc_probs = tmp_mc_probs / N / repetitions
+
+    !> For the SIR model, we must perform the following exchanges at the end of
+    !> the simulation: (pe, pi, pr) --> (0.0d0, pe, pi) and (E, I) --> (I, R).
     if (trim(model) == 'SIR') then
       tmp_mc_probs(:, 3:4) = tmp_mc_probs(:, 2:3)
       tmp_mc_probs(:, 2) = 0.0d0
 
-      do i = 1, N
-        if (states(i) == 'I') then
-          states(i) = 'R'
-        else if (states(i) == 'E') then
-          states(i) = 'I'
-        end if
+      do t = 0, t0
+        do i = 1, N
+          if (tmp_states(t, i) == 'I') then
+            tmp_states(t, i) = 'R'
+          else if (tmp_states(t, i) == 'E') then
+            tmp_states(t, i) = 'I'
+          end if
+        end do
       end do
     end if
   end subroutine mc_sim
