8a9d1189f1f566b445ea217442329f189801d14b
DrMeca <amecamon7@gmail.com>
2022-08-17 14:28:58 +0200

    The Monte Carlo history is no longer saved because it took up too much memory.

--- simulations/pz_simulation.f90
+++ simulations/pz_simulation.f90
@@ -1,7 +1,7 @@
 !> Simulation that tests the efficiency with which the DMP-based inference algorithm
 !> locates the true patient zero of an epidemic.
 !> Author: Adria Meca Montserrat.
-!> Last modified date: 14/08/22.
+!> Last modified date: 17/08/22.
 program pz_simulation
   use array_procedures,        only: find
   use derived_types,           only: int_llist, node, prm
@@ -12,24 +12,24 @@ program pz_simulation
 
   implicit none
 
-  integer,          allocatable :: nbrs(:)                                              !> Neighbors of node i at time t.
-  integer,          allocatable :: non_S(:)                                             !> Non-susceptible nodes.
-  integer                       :: alti, c, i, idx, instances, iseed, N, r0, t, t0, t1  !>
-  integer                       :: epi_size                                             !> Epidemic size.
-  integer                       :: patient_zeros(1)                                     !>
-  integer                       :: ppz, tpz                                             !>
-  integer                       :: values(8)                                            !>
-  character(len=1), allocatable :: states(:, :)                                         !> Distribution of node states.
-  character(len=9)              :: graph, model                                         !>
-  double precision, allocatable :: energies(:)                                          !> Node energies.
-  double precision, allocatable :: r(:, :)                                              !> Node positions.
-  double precision, parameter   :: small = 1.0d-300                                     !>
-  double precision              :: alpha, e, l, lambda, mu, nu, Q                       !>
-  logical                       :: restricted, randomized                               !>
-  type(int_llist),  allocatable :: indices(:)                                           !> Active links throughout the simulation.
-  type(node),       allocatable :: history(:)                                           !> Rewiring history.
-  type(node),       allocatable :: network(:)                                           !> Original network.
-  type(prm)                     :: epi_params                                           !> Epidemiological parameters.
+  integer,          allocatable :: non_S(:)                                  !> Non-susceptible nodes.
+  integer                       :: alti, c, i, idx, instances, iseed, N, r0  !>
+  integer                       :: epi_size                                  !> Epidemic size.
+  integer                       :: patient_zeros(1)                          !>
+  integer                       :: ppz, tpz                                  !>
+  integer                       :: t, t0, t1, t2                             !>
+  integer                       :: values(8)                                 !>
+  character(len=1), allocatable :: states(:, :)                              !> Distribution of node states.
+  character(len=9)              :: graph, model                              !>
+  double precision, allocatable :: energies(:)                               !> Node energies.
+  double precision, allocatable :: r(:, :)                                   !> Node positions.
+  double precision, parameter   :: small = 1.0d-300                          !>
+  double precision              :: alpha, e, l, lambda, mu, nu, Q            !>
+  logical                       :: restricted, randomized                    !>
+  type(int_llist),  allocatable :: indices(:)                                !> Active links throughout the simulation.
+  type(node),       allocatable :: history(:)                                !> Rewiring history.
+  type(node),       allocatable :: network(:)                                !> Original network.
+  type(prm)                     :: epi_params                                !> Epidemiological parameters.
 
   !> Parameters.
   open(unit=10, file='pz_simulation'//'.txt')
@@ -101,75 +101,37 @@ program pz_simulation
       end if
     end do
 
-    !> Monte Carlo history.
-    open(unit=15, file='pz_simulation'//'_mc.dat', position='append')
-      do t = 0, t0
-        do i = 1, N
-          allocate(nbrs(size(indices(i)%time(t)%array)))
-          nbrs = history(i)%neighbors(indices(i)%time(t)%array)
+    !> We compute the time t2 at which the true patient zero recovered.
+    do t = 0, t0
+      if (states(t, tpz) == 'R') then
+        t2 = t
+        exit
+      end if
+    end do
 
-          if (t == 0) then
-            write(15, '(3i10,a10,999i10,:)') idx, t, i, states(t, i), nbrs
-          else if (states(t-1, i) /= states(t, i)) then
-            write(15, '(3i10,a10,999i10,:)') idx, t, i, states(t, i), nbrs
-          else if (different_neighbors(history, indices, i, t)) then
-            write(15, '(3i10,a10,999i10,:)') idx, t, i, states(t, i), nbrs
-          end if
+    !> Energies.
+    open(unit=15, file='pz_simulation'//'_energies.dat', position='append')
+      epi_size = size(non_S)
+      do alti = 1, min(epi_size, 100)
+        e = energies(alti); i = non_S(alti)
 
-          deallocate(nbrs)
-        end do
+        if (e < 1/small) write(15, '(3i10,es26.16)') idx, epi_size, i, e
       end do
     close(15)
 
-    !> Energies.
-    open(unit=20, file='pz_simulation'//'_energies.dat', position='append')
-      epi_size = size(non_S)
-      do alti = 1, epi_size
-        e = energies(alti); i = non_S(alti)
-
-        if (e < 1/small) write(20, '(3i10,es26.16)') idx, epi_size, i, e
-      end do
-    close(20)
-
     !> Output.
-    write(*, '(10i10)')                                                             &
+    write(*, '(12i10)')                                                             &
       tpz,                                                                          &
       ppz,                                                                          &
       r0,                                                                           &
       t1,                                                                           &
+      t2,                                                                           &
       merge(1, 0, any(history(ppz)%neighbors(indices(ppz)%time(t1)%array) == tpz)), &
-      merge(1, 0, states(t1, tpz) == 'I'),                                          &
       count(states(t0, :) == 'I'),                                                  &
       count(states(t0, :) == 'R'),                                                  &
       count(states(t1, :) == 'I'),                                                  &
-      count(states(t1, :) == 'R')
+      count(states(t1, :) == 'R'),                                                  &
+      count(states(t2, :) == 'I'),                                                  &
+      count(states(t2, :) == 'R')
   end do
-
-contains
-  !> Indicates whether the neighbors of node i have changed since the previous time step.
-  function different_neighbors(history, indices, i, t)
-    integer,         intent(in) :: i                    !> Node of interest.
-    integer,         intent(in) :: t                    !> Time step.
-    integer                     :: altk, isize, k       !>
-    logical                     :: different_neighbors  !> Output.
-    type(int_llist), intent(in) :: indices(:)           !> Active links throughout the simulation.
-    type(node),      intent(in) :: history(:)           !> Rewiring history.
-
-    !> Number of active links of node i at time t.
-    isize = size(indices(i)%time(t)%array)
-
-    different_neighbors = .false.
-    if (isize /= size(indices(i)%time(t-1)%array)) then
-      different_neighbors = .true.
-    else
-      do altk = 1, isize
-        k = history(i)%neighbors(indices(i)%time(t)%array(altk))
-
-        if (all(history(i)%neighbors(indices(i)%time(t-1)%array) /= k)) then
-          different_neighbors = .true.
-          exit
-        end if
-      end do
-    end if
-  end function different_neighbors
 end program pz_simulation
